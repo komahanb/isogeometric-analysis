2948cc6b397248faa079a28a1361dd4f1bd55690
Komahan Boopathy <komahan.cool@gmail.com>
2019-04-20 23:39:14 -0400

    added linear shape functions

--- examples/axial-bar/basis.py
+++ examples/axial-bar/basis.py
@@ -5,7 +5,8 @@ def N(xi, i, enum):
         if i == 2:
             return -2*xi*(3*xi-2)
         if i == 3:
-            return 2*xi*xi
+            return 2*xi*xi        
+
     if enum == 2:
         if i == 1:
             return (2*xi-2)*(xi-1)
@@ -22,6 +23,7 @@ def Nprime(xi, i, enum):
             return 4*(1-3*xi)
         if i == 3:
             return 4*xi
+
     if enum == 2:
         if i == 1:
             return 4*(xi-1)
@@ -29,4 +31,17 @@ def Nprime(xi, i, enum):
             return 4*(2-3*xi)
         if i == 3:
             return 4*(2*xi-1)
+
+def H(xi, L, i):
+    if i == 1:
+        return 1.0 - xi/L 
+    if i == 2:
+        return xi/L
+
+def Hprime(xi, L, i):
+    if i == 1:
+        return -1.0/L 
+    if i == 2:
+        return 1.0/L
+
 
