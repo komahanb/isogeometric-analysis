b311a06f8333e0cb45e06f69986c86065b9402e5
komahan <komahan.cool@gmail.com>
2019-04-22 10:48:14 -0400

    implemented constitutive matrix

--- examples/plate-with-hole/constitutive_matrix.m
+++ examples/plate-with-hole/constitutive_matrix.m
@@ -1,3 +1,9 @@
-function [C] = constitutive_matrix()
-C = eye(3);
+function [C] = constitutive_matrix(E,nu,h)
+C = zeros(3,3);
+a = E*h*h*h/(12.0*(1-nu*nu));
+C(1,1) = a;
+C(1,2) = a*nu;
+C(2,1) = a*nu;
+C(2,2) = a;
+C(3,3) = 0.5*a*(1-nu);
 end
